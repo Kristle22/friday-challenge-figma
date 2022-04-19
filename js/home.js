ffd7eaadf7d46bec225e5bbfa94f6ca57717e026
Kristina <crislayn@yahoo.com>
2022-04-19 18:41:11 +0300

    JavaScript time formatting

--- js/home.js
+++ js/home.js
@@ -2,11 +2,16 @@ setInterval(myClock, 1000);
 
 function myClock() {
   const date = new Date();
-  document.getElementById("hours").innerHTML = date.toLocaleTimeString();
+  document.getElementById('hours').innerHTML = date.toLocaleTimeString('lt');
 }
 
 const date = new Date();
-document.getElementById("date").innerHTML = date.toDateString("");
+document.getElementById('date').innerHTML = date.toLocaleDateString('lt', {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'short',
+  day: 'numeric',
+});
 
-const hello = "Kristina";
-document.getElementById("myName").innerHTML = hello;
+const hello = 'Kristina';
+document.getElementById('myName').innerHTML = hello;
